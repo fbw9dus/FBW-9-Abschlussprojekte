47d5eec75efefca5fae7ad9448bf71b8dd06c421
kadirumac <kadirumac49@gmail.com>
2020-07-20 15:42:21 +0200

    Updates

---
+++ precache-manifest.b3489eb5ae3ae0db1e623d6da05eee34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418387f3354077a4fafa8571c8628534",
+    "url": "/FBW-9-Abschlussprojekte/index.html"
+  },
+  {
+    "revision": "f490b148dc496b9b2e0b",
+    "url": "/FBW-9-Abschlussprojekte/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c6e01c8ee4c1d064ed7b",
+    "url": "/FBW-9-Abschlussprojekte/static/css/main.fce77ea0.chunk.css"
+  },
+  {
+    "revision": "f490b148dc496b9b2e0b",
+    "url": "/FBW-9-Abschlussprojekte/static/js/2.11238beb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FBW-9-Abschlussprojekte/static/js/2.11238beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e01c8ee4c1d064ed7b",
+    "url": "/FBW-9-Abschlussprojekte/static/js/main.4b74f438.chunk.js"
+  },
+  {
+    "revision": "e319bbe73ab83fd07ef5",
+    "url": "/FBW-9-Abschlussprojekte/static/js/runtime-main.056e2e1b.js"
+  }
+]);
